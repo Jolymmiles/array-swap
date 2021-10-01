fcc47d97d53a92bac4675cec972847fc082cd90a
Валентин <valmak04@gmail.com>
2021-10-01 18:19:25 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //write your code here
+        Scanner sc = new Scanner(System.in);
+        String input_data = sc.nextLine();
+        String[] spec = input_data.split(" ");
+        int n = Integer.parseInt(spec[0]), a = Integer.parseInt(spec[1]), b = Integer.parseInt(spec[2]);
+        int[] array = new int[n];
+        for (int count = 0; count < n; count += 1){
+            array[count] = sc.nextInt();
+        }
+        int time_elem = array[a];
+        array[a] = array[b];
+        array[b] = time_elem;
+        for (int elem : array) System.out.println(elem);
+
+
+
     }
 }
